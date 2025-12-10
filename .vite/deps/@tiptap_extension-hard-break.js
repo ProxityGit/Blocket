6b9d1d02ba28ebe0b16922aa78b33906f4cf67e6
Proxity <global@proxity.com.co>
2025-12-09 22:27:34 -0500

    Restaurar estructura original del proyecto y archivos frontend

---
+++ .vite/deps/@tiptap_extension-hard-break.js
@@ -0,0 +1,10 @@
+import {
+  HardBreak,
+  index_default
+} from "./chunk-PTP552OE.js";
+import "./chunk-UKPQ2PAV.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  HardBreak,
+  index_default as default
+};
---
+++ .vite/deps/@tiptap_extension-table-cell.js
@@ -0,0 +1,13 @@
+import {
+  TableCell
+} from "./chunk-VF6LGHKZ.js";
+import "./chunk-UKPQ2PAV.js";
+import "./chunk-G3PMV62Z.js";
+
+// node_modules/@tiptap/extension-table-cell/dist/index.js
+var index_default = TableCell;
+export {
+  TableCell,
+  index_default as default
+};
+//# sourceMappingURL=@tiptap_extension-table-cell.js.map
